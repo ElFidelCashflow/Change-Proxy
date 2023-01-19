ea42516ad578daa77157bacf05b56478b5634b1d
Thibault Oger <thibaut.oger49@gmail.com>
2023-01-19 18:53:07 +0100

    Add sudo call to execute as root

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,18 @@ use std::error::Error;
 use std::process;
 
 use clap::Parser;
-use tracing::{info, Level};
+use tracing::{info, trace, Level, warn};
 use tracing_subscriber::FmtSubscriber;
 mod libs;
 
 fn main() {
     let args = libs::args::Cli::parse();
+    if let Err(_) = run(args) {
+        process::exit(1);
+    }
+}
 
+fn run(args: libs::args::Cli) -> Result<(), Box<dyn Error>> {
     let verbosity: Level;
     match args.verbosity {
         0 => verbosity = Level::INFO,
@@ -19,19 +24,18 @@ fn main() {
     let subscriber = FmtSubscriber::builder()
         // all spans/events with a level higher than TRACE (e.g, debug, info, warn, etc.)
         // will be written to stdout.
+        .compact()
         .with_max_level(verbosity)
         // completes the builder.
         .finish();
 
     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+
+    sudo::with_env(&["HOME"])?;
+    warn!("Running as {:?}", sudo::check());
+    trace!("Env HOME : {}", std::env::var("HOME").unwrap());
+
     info!("Usinge {verbosity} level of log");
-
-    if let Err(_) = run(args) {
-        process::exit(1);
-    }
-}
-
-fn run(args: libs::args::Cli) -> Result<(), Box<dyn Error>> {
     libs::vscode::manage_proxy(&args.command)?;
     Ok(())
 }
